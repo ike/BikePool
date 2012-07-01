98e846c8da7692233c5e675c1a44605159ab0410
Isaac Lewis <ikelewis678@gmail.com>
2012-07-01 13:10:17 -0400

    ALL THE THINGYS

--- routes/bikepool.js
+++ routes/bikepool.js
@@ -11,10 +11,46 @@ var routes = {
 	4321:{name:'Sunset Park to Chelsea', start:'Sunset Park, NY, USA', end:'Chelsea, NY, USA', members:567},
 }
 
-var times = [
-	'6:00',
-	'6:30',
-]
+var times = {
+	600:{time:'6:00'},
+	630:{time:'6:30'},
+	700:{time:'7:00'},
+	730:{time:'7:30'},
+	800:{time:'8:00'},
+	830:{time:'8:30'},
+	900:{time:'9:00'},
+	930:{time:'9:30'},
+	1000:{time:'10:00'},
+	1030:{time:'10:30'},
+	1100:{time:'11:00'},
+	1130:{time:'11:30'},
+	1200:{time:'12:00'},
+	1230:{time:'12:30'},
+	1300:{time:'1:00'},
+	1330:{time:'1:30'},
+	1400:{time:'2:00'},
+	1430:{time:'2:30'},
+	1500:{time:'3:00'},
+	1530:{time:'3:30'},
+	1600:{time:'4:00'},
+	1630:{time:'4:30'},
+	1700:{time:'5:00'},
+	1730:{time:'5:30'},
+	1800:{time:'6:00'},
+	1830:{time:'6:30'},
+	1900:{time:'7:00'},
+	1930:{time:'7:30'},
+	2000:{time:'8:00'},
+	2030:{time:'8:30'},
+	2100:{time:'9:00'},
+	2130:{time:'9:30'},
+	2200:{time:'10:00'},
+	2230:{time:'10:30'},
+	2300:{time:'11:00'},
+	2330:{time:'11:30'},
+	0000:{time:'12:00'},
+	0030:{time:'12:30'}
+}
 
 // handler for displaying the routes
 exports.routes = function(req, res) {
@@ -26,5 +62,5 @@ exports.route = function(req, res) {
     var name = routes[req.params.id].name;
     var start = routes[req.params.id].start;
     var end = routes[req.params.id].end;
-    res.render('route', { title: name + ' - BikePool', name:name, start:start, end:end });
+    res.render('route', { title: name + ' - BikePool', name:name, start:start, end:end, times.times });
 };
